ed598e2176bec99935dd63c2b18715ba2bf3019d
Fall-crystl <fallcrystl@gmail.com>
2022-01-09 15:40:30 +0100

    add spacing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 import './App.css';
-import {CardList} from './components/card-list/card-list.component';
-import {SearchBox} from './components/search-box/search-box.component';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 class App extends Component {
   constructor() {
